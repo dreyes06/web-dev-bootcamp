344b0dc99ab0ed49975873bdd48d2f58b43943b0
Daniel <djreyes042497@gmail.com>
2021-01-05 21:03:51 -0600

    first commit

---
+++ drum-kit/index.js
@@ -0,0 +1,8 @@
+for (var i = 0; i < document.querySelectorAll('.drum').length; i++){
+    document.querySelectorAll('.drum').addEventListener('click', function() {
+        alert('clicked')
+    })
+}
+
+
+
