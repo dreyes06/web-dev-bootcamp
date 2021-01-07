e7b0ee45a5bf8c52d6af50201974ccb6b3611ceb
Daniel <djreyes042497@gmail.com>
2021-01-07 13:58:06 -0600

    background images added

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,6 +1,6 @@
-for (var i = 0; i < document.querySelectorAll('.drum').length; i++){
-    document.querySelectorAll('.drum').addEventListener('click', function() {
-        alert('clicked')
+for (let i = 0; i < document.querySelectorAll('.drum').length; i++){
+    document.querySelectorAll('.drum')[i].addEventListener('click', function() {
+        this.style.color = 'white'
     })
 }
 
